ebe64dd68b143616bd65528e6d7f8bba7519743f
Andréia Leite <108141128+xAndreiaLeite@users.noreply.github.com>
2022-07-20 17:28:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let lista = 10;
+
+for(num = 0; num <= lista ; num ++){
+   if(num == 0) {
+    console.log(num + " é zero");
+   }  
+   else if(num % 2 === 0 ) {
+    console.log(num + " número é par");
+   } 
+   else{
+    console.log( num + " número é ímpar" );
+   }
+}
